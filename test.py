f17f8568306143dd375e44f5ad2323ed8d499ca9
Cole Burley <cburley@bgsu.edu>
2020-08-06 02:43:13 -0400

    Testing for keeping unique stem words added

--- test.py
+++ test.py
@@ -3,13 +3,22 @@ from numpy import array
 import nltk
 from nltk.stem import PorterStemmer
 from nltk.tokenize import RegexpTokenizer
+from nltk.corpus import stopwords
 
 ps = PorterStemmer()
 tk = RegexpTokenizer(r'\w+')
+stopWords = set(stopwords.words('english'))
+testSentence = "I am searching. I have searched. You will search. You will be looking, as I have looked."
+testSentenceLower = testSentence.lower()
+uniqueTokens = set()
 
-testSentence = "searching searched search looking, looked"
-
-testWordsTokenized = tk.tokenize(testSentence)
-
-for w in testWordsTokenized:
-    print(w, " ", ps.stem(w))
+testWordsTokenized = tk.tokenize(testSentenceLower)
+testWordStop = [word for word in testWordsTokenized if word not in stopWords]
+testWordStem = [ps.stem(word) for word in testWordStop]
+testWordUnique = array([])
+for word in testWordStem:
+    if word not in uniqueTokens:
+        uniqueTokens.add(word)
+        testWordUnique = numpy.append(word, testWordUnique)
+for w in testWordUnique:
+    print(w)
