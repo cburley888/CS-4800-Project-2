e05efe9f6e5f29c7e5011f5a73187b6474bb934b
Cole Burley <cburley@bgsu.edu>
2020-08-09 21:10:14 -0400

    The document vector is now being properly populated with term frequencies

--- makeVector.py
+++ makeVector.py
@@ -9,12 +9,14 @@ np.set_printoptions(threshold=sys.maxsize)
 
 def make_vector(document, vocabList):
     doc_vector = np.zeros(len(vocabList))
+    print(doc_vector)
     documentTokenized = tokenizer.tokenizer(document)
     documentStopWord = stopWordRemover.stopwordremover(documentTokenized)
     documentStemmed = stemmer.stemmer(documentStopWord)
-    """for word in documentStemmed:
-        doc_vector[np.where(vocabList == word)] += 1"""
-    for word in documentStemmed:
-        print(np.where(vocabList == word))
+    document_filtered = [word for word in documentStemmed if word in vocabList]
+    for word in document_filtered:
+        doc_vector[vocabList[word]] += 1
+    """for word in vocabList:
+        print(word)"""
 
     return doc_vector
--- vocabCreator.py
+++ vocabCreator.py
@@ -9,5 +9,10 @@ def vocabcreator(string):
     tokenized = tokenizer.tokenizer(lower)
     stopwords = stopWordRemover.stopwordremover(tokenized)
     stem = stemmer.stemmer(stopwords)
-    uniquetokens = uniqueTokenFilter.uniquetokenfilter(stem)
-    return uniquetokens
+    unique_tokens = uniqueTokenFilter.uniquetokenfilter(stem)
+    vocab = {}
+    i = 0
+    for word in unique_tokens:
+        vocab.update({word: i})
+        i += 1
+    return vocab
