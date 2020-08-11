3f15238442a6b5b376558d51af4ca8a44d9fbaea
cburley <cburley@bgsu.edu>
2020-08-11 03:02:03 -0400

    Project completed and working

--- docProcessing.py
+++ docProcessing.py
@@ -7,6 +7,6 @@ import uniqueTokenFilter
 def process_doc(string, vocabList):
     documentTokenized = tokenizer.tokenizer(string)
     documentStopWord = stopWordRemover.stopwordremover(documentTokenized)
-    documentStemmed = stemmer.stemmer(documentTokenized)
+    documentStemmed = stemmer.stemmer(documentStopWord)
     document_filtered = [word for word in documentStemmed if word in vocabList]
     return document_filtered
--- vocabCreator.py
+++ vocabCreator.py
@@ -8,7 +8,7 @@ def vocabcreator(string):
     lower = string.lower()
     tokenized = tokenizer.tokenizer(lower)
     stopwords = stopWordRemover.stopwordremover(tokenized)
-    stem = stemmer.stemmer(tokenized)
+    stem = stemmer.stemmer(stopwords)
     unique_tokens = uniqueTokenFilter.uniquetokenfilter(stem)
     vocab = {}
     i = 0
